08005dc5715a5f89962f83f1ad77d46a0f7c9463
Saurabh Salve <saurabh.salve7@gmail.com>
2020-04-29 19:49:56 +0530

    Comment Removed

--- src/main/java/com/cg/entity/Customer.java
+++ src/main/java/com/cg/entity/Customer.java
@@ -12,7 +12,7 @@ import javax.persistence.OneToMany;
 
 @Entity
 public class Customer implements Serializable{
-	//Test Commet
+	
 	private static final long serialVersionUID = 1L;
 	private String name;
 	@Id
